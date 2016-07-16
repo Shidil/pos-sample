89a70c35fbb5b340fb66f627266355b37a71ab97
Shidil Eringa <shidil@live.com>
2016-07-16 17:49:03 +0530

    Added normalize.less

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import RouterContainer from './routes';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import { useRouterHistory } from 'react-router';
+import styles from './styles/base.less'; // eslint-disable-line no-unused-vars
 
 const MOUNT_NODE = document.getElementById('root');
 const history = useRouterHistory(createBrowserHistory)({
