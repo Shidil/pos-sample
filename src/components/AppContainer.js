137082f4fa269911d3e327ab89c7d27bf8ac8d29
Shidil Eringa <shidil@live.com>
2016-07-16 22:36:55 +0530

    Added some nav links

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link, IndexLink } from 'react-router';
 import styles from './AppContainer.less'; // eslint-disable-line no-unused-vars
 
 class AppContainer extends React.Component {
   render() {
     return (
       <div className="main-view">
+        <header className="header">
+          <nav className="header-nav">
+            <IndexLink to="/">Home</IndexLink>
+            {' | '}
+            <Link to="/about">About</Link>
+            {' | '}
+            <Link to="/dead">Dead-Page</Link>
+          </nav>
+        </header>
         {this.props.children}
       </div>
     );
