0c3629b02fa6f1355f3c197988d7991e460fad47
Shidil Eringa <shidil@live.com>
2017-05-04 20:15:12 +0530

    Update styles to be responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,19 +5,53 @@ import PropTypes from 'prop-types';
 import './Navbar.less';
 
 class NavbarComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuVisible: false
+    };
+    this.showMenu = this.showMenu.bind(this);
+  }
+
+  showMenu() {
+    this.setState({
+      menuVisible: !this.state.menuVisible
+    });
+  }
+
   render() {
     if (!this.props.isLoggedIn) {
       return null;
     }
 
+    const sidebarClassList = `navbar-sidebar ${this.state.menuVisible ? '' : 'hidden'}`;
+
     return (
-      <nav className="sidebar-nav">
-        <Link to="/sell" className="nav-item logo" />
-        <Link to="/sell" className="nav-item material-icons">shopping_cart</Link>
-        <Link to="/settings" className="nav-item material-icons">settings</Link>
-        <Link to="/logout" className="nav-item material-icons">lock</Link>
-        <Link to="/about" className="nav-item material-icons">info</Link>
-      </nav>
+      <div>
+        <header className="navbar-header">
+          <button className="fa fa-bars pull-left" onClick={this.showMenu}/>
+          <Link to="/sell" className="header-logo">Till</Link>
+        </header>
+        <nav className={sidebarClassList}>
+          <Link to="/sell" className="nav-item logo" />
+          <Link to="/sell" className="nav-item">
+            <i className="material-icons">shopping_cart</i>
+            <span className="nav-item-title">Sell</span>
+          </Link>
+          <Link to="/settings" className="nav-item">
+            <i className="material-icons">settings</i>
+            <span className="nav-item-title">Settings</span>
+          </Link>
+          <Link to="/logout" className="nav-item">
+            <i className="material-icons">lock</i>
+            <span className="nav-item-title">Logout</span>
+          </Link>
+          <Link to="/about" className="nav-item">
+            <i className="material-icons">info</i>
+            <span className="nav-item-title">About</span>
+          </Link>
+        </nav>
+      </div>
     );
   }
 }
